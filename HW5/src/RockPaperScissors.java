0451da3d9545acda13d454a27f2ffde30489d977
tf2276714 <Trevis42@Trevis-MacBook-Pro.local>
2015-10-14 14:01:07 -0700

    Homework
    Homework 5 and 6 uploaded but need to be redone to retain the material

---
+++ HW5/src/RockPaperScissors.java
@@ -0,0 +1,50 @@
+//Written By Trevis Fields
+//Rock Paper Scissors
+//CIS 18A Mon/Wed 2:20- 3:45 pm
+//Homework Game Zone #5 pg 347
+//October 14 2015
+
+import javax.swing.JOptionPane;
+
+public class RockPaperScissors {
+	public static void main(String[] args) {
+		int rock = 1;
+		int paper = 2;
+		int scissors = 3;
+		String choice;
+		int userChoice;
+		int randomValue;
+
+		choice = JOptionPane.showInputDialog(null,
+				"Please choose between 1 (rock), 2 (paper), and 3 (scissors).");
+		userChoice = Integer.parseInt(choice);
+
+		randomValue = ((int) (Math.random() * 3)+1);
+		System.out.println(randomValue);
+
+		if (userChoice == randomValue) {
+			JOptionPane.showMessageDialog(null, "It is a tie!");
+		} else if (userChoice == rock && randomValue == paper) {
+			JOptionPane.showMessageDialog(null, "Paper covers rock! You lose!");
+		} else if (userChoice == rock && randomValue == scissors) {
+			JOptionPane.showMessageDialog(null, "Rock smashes scissors! You win!");
+		}
+
+		else if (userChoice == paper && randomValue == rock) {
+			JOptionPane.showMessageDialog(null, "Paper beats rock! You win!");
+		}
+
+		else if (userChoice == paper && randomValue == scissors) {
+			JOptionPane.showMessageDialog(null, "Scissors cuts paper! You lose!");
+		}
+
+		else if (userChoice == scissors && randomValue == rock) {
+			JOptionPane.showMessageDialog(null, "Rock smashes scissors! You lose!");
+		}
+
+		else if (userChoice == scissors && randomValue == paper) {
+			JOptionPane.showMessageDialog(null, "Scissors cuts paper! You win!");
+		}
+
+	}
+}
---
+++ HW6/src/Count21.java
@@ -0,0 +1,103 @@
+//Written by Trevis Fields
+//Written on 10-7-15
+//CIS 18A Mon/Wed 2:20- 3:45 pm
+
+
+import javax.swing.JOptionPane;
+
+
+public class Count21 
+{
+	public static void main(String[] args) 
+	{
+		
+	//datatype(s) and variable
+		int            userChoice = 0;
+		int            cpuChoice = 1;
+		int            total = 0;
+		
+		boolean        whenGameOver = false;
+		
+		//Class and object
+		String         userInput;
+		
+		total += cpuChoice;
+		JOptionPane.showMessageDialog(null, "Welcome to 21.\nTry not to go over 21.");
+		JOptionPane.showMessageDialog(null, "The Computer starts the game and selects: " + cpuChoice);
+		
+       
+		do 
+		{
+			userInput = JOptionPane.showInputDialog(null, "Enter a number from 1 to 3.");
+			userChoice = Integer.parseInt(userInput); //Convert string object to integer
+			
+			//testing for wrong choices
+			if(userChoice >= 4 || userChoice <= 0)
+			{
+				JOptionPane.showMessageDialog(null, "Invalid Entry");
+			}
+			else if(userChoice == 1)
+			{
+				//If the total is 21 and above, set isGameOver to true
+				if(total >= 21)
+				{
+					JOptionPane.showMessageDialog(null, "Sorry but you lose!");
+					whenGameOver = true;
+				}
+				else {
+				JOptionPane.showMessageDialog(null, "Your hand: " + userChoice);
+				total +=userChoice;
+				JOptionPane.showMessageDialog(null, "The current total: " + total);
+				cpuChoice = 3;
+				JOptionPane.showMessageDialog(null, "Computer pick: " + cpuChoice);
+				total += cpuChoice;
+				JOptionPane.showMessageDialog(null, "The current total: " + total);
+				}
+			}
+			
+			else if(userChoice == 2)
+			{
+				//If the total is 21 and above, set whenGameOver to true
+				if(total >= 21)
+				{
+					JOptionPane.showMessageDialog(null, "You Lose FOOL!");
+					whenGameOver = true;
+				}
+				else {
+				JOptionPane.showMessageDialog(null, "You chose " + userChoice);
+				total +=userChoice;
+				JOptionPane.showMessageDialog(null, "The current total is " + total);
+				cpuChoice = 2;
+				JOptionPane.showMessageDialog(null, "CPU chose " + cpuChoice);
+				total += cpuChoice;
+				JOptionPane.showMessageDialog(null, "The current total is " + total);
+				}
+			}
+				
+			else if(userChoice == 3)
+			{
+                //copied from above code with a few changes
+				if(total >= 21)
+					
+				{
+					JOptionPane.showMessageDialog(null, "You Lose FOOL!");
+					whenGameOver = true;
+				}
+				else {
+				JOptionPane.showMessageDialog(null, "You chose " + userChoice);
+				total +=userChoice;
+				JOptionPane.showMessageDialog(null, "The current total is " + total);
+				cpuChoice = 1;
+				JOptionPane.showMessageDialog(null, "CPU chose " + cpuChoice);
+				total += cpuChoice;
+				JOptionPane.showMessageDialog(null, "The current total is " + total);
+				}
+			}
+		
+			
+			System.out.println("Test");
+			
+		}while (whenGameOver == false);
+		
+	}
+}
