b413b31e9699e0febd588f5d08c05739526a42b9
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2016-09-28 17:53:05 -0700

    Homework 04

--- HW_04/src/hw_04/TestState.java
+++ HW_04/src/hw_04/TestState.java
@@ -1,7 +1,15 @@
-
 package hw_04;
 
 
-public class TestState {
+
+public class TestState 
+{
+    public static void main(String[] args)
+    {
+        //Class     object = new Constructor method
+        State california = new State("CALIFORNIA", 1000000,
+                "Sacramento", 62, "Moreno Valley", 39495966);
+        california.displayCityInfo();
+    }
     
 }
