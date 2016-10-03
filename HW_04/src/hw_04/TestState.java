cbbccae8b148e2c85a62a7e7c569488b0091e17b
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2016-10-03 15:39:54 -0700

    Chapter_4_Problem_11
    State.Java and TestState.java

--- HW_04/src/hw_04/TestState.java
+++ HW_04/src/hw_04/TestState.java
@@ -8,16 +8,26 @@ public class TestState
     {
         //Class     object = new Constructor method
         State california = new State("CALIFORNIA", 1046753,
-                "Sacramento", 700000, "Moreno Valley", 25000);
+                "Sacramento", 700000, "MORENO VALLEY", 25000);
         california.displayCityInfo();
         
         State texas = new State("TEXAS", 2345231,
-                "Houston", 54000, "Austin", 800000);
+                "HOUSTON", 54000, "AUSTIN", 800000);
         texas.displayCityInfo();
         
+        /*
         State arizona = new State("Arizona", 10039456,
-                "Phoneix", 150000, "Tucson", 70200);
+                "PHONEIX", 150000, "TUCSON", 70200);
         arizona.displayCityInfo();
+        */
+        
+        /*
+        State newYork = new State("NEW YORK", 345029343,
+                "ALBANY", 500000, "BUFFALO", 70001923);
+        newYork.displayCityInfo();
+        */
+        
+        
     }
     
 }
