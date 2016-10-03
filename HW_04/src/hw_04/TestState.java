c013d5b0ec5a616e830a6b24b0ee2fa9981bf9b1
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2016-10-03 10:20:15 -0700

    HW04
    Hw 04 finished

--- HW_04/src/hw_04/TestState.java
+++ HW_04/src/hw_04/TestState.java
@@ -7,9 +7,17 @@ public class TestState
     public static void main(String[] args)
     {
         //Class     object = new Constructor method
-        State california = new State("CALIFORNIA", 1000000,
-                "Sacramento", 62, "Moreno Valley", 39495966);
+        State california = new State("CALIFORNIA", 1046753,
+                "Sacramento", 700000, "Moreno Valley", 25000);
         california.displayCityInfo();
+        
+        State texas = new State("TEXAS", 2345231,
+                "Houston", 54000, "Austin", 800000);
+        texas.displayCityInfo();
+        
+        State arizona = new State("Arizona", 10039456,
+                "Phoneix", 150000, "Tucson", 70200);
+        arizona.displayCityInfo();
     }
     
 }
